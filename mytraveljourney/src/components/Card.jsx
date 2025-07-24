46ab37396f81850324c0fc397d1f5e8ffadda8a2
ramesh-1998-coder <85332390+ramesh-1998-coder@users.noreply.github.com>
2025-07-24 17:18:22 -0400

    sync changes

--- mytraveljourney/src/components/Card.jsx
+++ mytraveljourney/src/components/Card.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import loactionicon from "../images/redicon.jpg";
 
 export default function Card(props){
+    const [isShown,setIsShown] = React.useState(false)
+    function handleClick(){
+        setIsShown(value => !value)
+        console.log(isShown)
+    }
+
+    // state and function for conditional rendering
+    const [messages, setMessages] = React.useState(["a"])
+
     return (
         <div className="card">
             <img src={props.img.src} alt={props.img.alt} className="card--image"/>
@@ -13,7 +22,10 @@ export default function Card(props){
                 </div>
                 <h1 className="card--title">{props.title}</h1>
                 <p className="card--dates">{props.dates}</p>
-                <p className="card--text">{props.text}</p>
+                {isShown && <p className="card--text">{props.text}</p>}
+                {messages.length===0 ? <h1>You're all caught up! </h1>
+                 : <h1>You have {messages.length} unread message{messages.length==1? "":'s'} </h1>}
+                <button onClick={handleClick}>{!isShown? "Show Description" : "Hide Description"}</button>
             </div>
         </div>
     )
